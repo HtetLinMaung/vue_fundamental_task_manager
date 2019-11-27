842c3bff66ae552f7046f99d0ed89f647a4d8352
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2019-11-27 11:00:15 +0630

    update

---
+++ vue_fundamental_tm_pre/src/components/SelectInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <select v-model="choice" class="select-box">
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+        >{{ option.text }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectInput",
+  props: ["options", "selected"],
+  data() {
+    return {
+      choice: this.selected
+    };
+  }
+};
+</script>
+
+<style scoped>
+.select-box {
+  font-size: 15px;
+  padding: 0.3rem 0.8rem;
+  color: rgb(117, 105, 105, 0.9);
+  border-radius: 4px;
+  border: 1px solid rgb(143, 137, 137);
+}
+</style>

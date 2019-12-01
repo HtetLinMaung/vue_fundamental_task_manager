15cfe17a242e84eae3c6f95d6086c9627bbfd53f
Jst <jsthtet96@gmail.com>
2019-12-01 08:10:48 +0630

    fix

---
+++ vue_fundamental_tm_pre/src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container" :style="{ maxWidth: `${maxWidth}px` }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container",
+  props: ["max-width"]
+}
+</script>
+
+<style scoped>
+.container {
+  margin: auto;
+}
+</style>
